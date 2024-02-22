18c3b35583cc76d9f2cdb264b2cdfda0de1a81ed
Pillber <melcard16@gmail.com>
2024-02-21 23:24:08 -0600

    player ability planning

--- deck.gd
+++ deck.gd
@@ -39,6 +39,28 @@ class Card:
 		1 Sensor - Any Time
 		1 Mutation - Any Time
 """
+"""
+	Player Abilities
+	Human:
+		Captain - First move is always sedative
+		Pilot - One time Cat
+		Psychologist - Start in Alien Spawn
+		Soldier - One time Attack
+		Executive Officer - "Lurk" (Stay still) once
+		Co-Pilot - One time Teleport
+		Engineer - Draw two escape cards, use one reshuffle other
+		Medic - Force reveal identity
+	Alien:
+		Blink - Can use Teleport
+		Silent - can use Sedatives
+		Surge - Can use Adrenaline
+		Brute - Immune to Attacks (reveal when attacked)
+		Invisible - Immune to Spotlight and Sensor (reveal when targeted)
+		Lurking - Attack without moving (still do sector stuff?)
+		Fast - Move 3 spaces first turn
+		Psychic - Every Silence is Noise in Any Sector
+
+"""
 
 
 var noise_deck_params = {
